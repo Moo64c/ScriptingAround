014112882e773e3631bd412266f256756911a20f
Amir Arbel <amirarbel@gmail.com>
2015-09-22 15:46:11 +0300

    Client.

--- client/app/scripts/config.js
+++ client/app/scripts/config.js
@@ -1,3 +1,6 @@
 angular.module('config', [])
 
-.constant('Config', {backend:'http://localhost/ScriptingAround/Drupal/www',debugUiRouter:true});
+  // Switch these when building.
+.constant('Config', {backend:'http://localhost/ScriptingAround/Drupal/www',debugUiRouter:true})
+  //.constant('Config', {backend:'http://php-rotternews.rhcloud.com/',debugUiRouter:true})
+;
